bd3c71c8557cb9462a764628c6b044a2c05615b6
Max Duval <max.duval@theguardian.com>
2024-05-08 13:01:28 +0100

    fix: improve as assertion

--- with-as/main.ts
+++ with-as/main.ts
@@ -4,14 +4,18 @@ import { init } from '../init.ts';
 init((json) => {
 	const data = json as RawCapiResponse;
 
-	return data.response.results.map((result) => ({
-		...result,
-		webPublicationDate: new Date(result.webPublicationDate),
-		webUrl: new URL(result.webUrl),
-		apiUrl: new URL(result.apiUrl),
-		fields: {
-			...result.fields,
-			thumbnail: new URL(result.fields.thumbnail),
-		},
-	}));
+	try {
+		return data.response.results.map((result) => ({
+			...result,
+			webPublicationDate: new Date(result.webPublicationDate),
+			webUrl: new URL(result.webUrl),
+			apiUrl: new URL(result.apiUrl),
+			fields: {
+				...result.fields,
+				thumbnail: new URL(result.fields.thumbnail),
+			},
+		}));
+	} catch (error) {
+		return error instanceof Error ? [error] : [Error('Unkown error')];
+	}
 });
